5d6610781ec45179582246d5ac0d13431ac0b9b6
mitchel buyengo <mitchelongachi63@gmail.com>
2023-08-24 10:41:41 +0300

    work done

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
+
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
@@ -11,8 +12,18 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home 
+      name={user.name}
+      color={user.color}
+      city={user.city}
+      
+      />
+      <About
+      linkedin = {user.links.linkedin}
+      github = {user.links.github}
+      bio = {user.bio}
+      />
+      <About/>
     </div>
   );
 }
